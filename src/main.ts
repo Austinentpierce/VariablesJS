6fb70a8aff443963d3e3891618b35c1beb193b9d
Austin Pierce <Austinentpierce@gmail.com>
2021-08-30 13:46:11 -0400

    added array into object

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
 import "./style.css";
+
+
+const numberOfCupsCoffee = 2
+const fullName = 'Austin Pierce'
+
+console.log(`I drink ${numberOfCupsCoffee} cups of coffee and my name is ${fullName}`)
+
+const aboutMe = {
+  fullName: 'Austin Pierce',
+  luckyNumber: 47, 
+  favoriteMovies: ['Wolf of Wallstreet' , 'Social Network' , 'Hangover' , 'Lone Survivor'],
+}
+// console.log(aboutMe)
+console.log(`My name is ${aboutMe.fullName} and my luck number is ${aboutMe.luckyNumber} and my favorite movies are ${aboutMe.favoriteMovies}`)
